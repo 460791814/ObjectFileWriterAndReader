e06adedf5d62083d22c4c5400c4793367bfea23a
460791814 <460791814@qq.com>
2016-11-13 19:26:23 +0800

    1

---
+++ Assets/TextScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void FixedUpdate()
+	{
+
+		if (Input.GetMouseButtonDown (0)) {
+			//刚体操作要在fixedupdate中进行操作
+			rigidbody.AddForce(new Vector2(200f,300f));
+		}
+
+	}
+
+	/// <summary>
+	/// 碰撞
+	/// </summary>
+	/// <param name="col"></param>
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		 if (col.collider) {
+				
+			print(1);
+		}
+
+	}
+}
